5f9c50586689aec95526773782f9dfdc5a2cf7b7
Austin Higgins <austinjhiggins@pm.me>
2025-04-02 09:34:57 -0230

    Changed url to steam login

--- src/components/SteamLogin.jsx
+++ src/components/SteamLogin.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const SteamLogin = ({ onSteamIdReceived }) => {
   const handleSteamLogin = () => {
-    window.location.href = '/.netlify/functions/steam-login';
+    window.location.href = "./src/services/steam-login.js";
   };
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const steamId = urlParams.get('steamId');
-    const error = urlParams.get('error');
-    
+    const steamId = urlParams.get("steamId");
+    const error = urlParams.get("error");
+
     if (steamId) {
-      localStorage.setItem('steamId', steamId);
+      localStorage.setItem("steamId", steamId);
       onSteamIdReceived(steamId);
       // Clear the URL parameters
       window.history.replaceState({}, document.title, window.location.pathname);
     } else if (error) {
-      console.error('Authentication failed:', error);
+      console.error("Authentication failed:", error);
       // Handle the error (e.g., show an error message to the user)
     }
   }, [onSteamIdReceived]);
 
   return (
-    <img 
-      onClick={handleSteamLogin} 
-      src="https://community.fastly.steamstatic.com/public/images/signinthroughsteam/sits_01.png" 
-      alt="Sign in with Steam" 
-      className='cursor-pointer' 
+    <img
+      onClick={handleSteamLogin}
+      src="https://community.fastly.steamstatic.com/public/images/signinthroughsteam/sits_01.png"
+      alt="Sign in with Steam"
+      className="cursor-pointer"
     />
   );
 };
